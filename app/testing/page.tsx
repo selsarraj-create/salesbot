7567543fc7676906d2770c8f004871aee021fde3
selsarraj-create <s.elsarraj@gmail.com>
2026-03-06 19:08:19 +0000

    fix: change sandbox info banner from unreadable yellow to blue

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -55,16 +55,16 @@ export default function TestingPage() {
     return (
         <div className="min-h-screen bg-panel-bg p-6">
             {/* Warning Banner */}
-            <div className="mb-6 bg-yellow-500/10 border-l-4 border-yellow-500 p-4 rounded">
+            <div className="mb-6 bg-blue-50 border-l-4 border-blue-500 p-4 rounded-lg">
                 <div className="flex">
                     <div className="shrink-0">
-                        <svg className="h-5 w-5 text-yellow-500" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                        <svg className="h-5 w-5 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
                         </svg>
                     </div>
                     <div className="ml-3">
-                        <h3 className="text-sm font-medium text-yellow-400">Testing Sandbox Mode</h3>
-                        <div className="mt-2 text-sm text-yellow-300">
+                        <h3 className="text-sm font-semibold text-blue-700">Testing Sandbox Mode</h3>
+                        <div className="mt-1 text-sm text-blue-600/80">
                             <p>
                                 All leads created here are marked as test leads. They will NOT trigger actual SMS messages or incur Twilio costs.
                             </p>
